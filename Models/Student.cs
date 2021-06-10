5360968fc35e37988cde233d538f5e6f96efda26
Davor Burmudžija <davorburmudzija@gmail.com>
2021-06-10 21:23:24 +0200

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PopisStudenata.Models
+{
+    [Table("Students")]
+
+    public class Student
+    {
+        [Key]
+
+        public string StudentID { get; set; }
+        [StringLength(100)]
+
+        public string FullName { get; set; }
+        [StringLength(100)]
+
+        public string Email { get; set; }
+        [StringLength(255)]
+
+        public string Address { get; set; }
+    }
+}
